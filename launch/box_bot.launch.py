a03a496f5c13fa81fed403d8cac6dea718ec4f6e
Rhody <rhodyboland@hotmail.com>
2024-07-25 14:44:28 +1000

    Fixed layout, accidental nested packages

--- launch/box_bot.launch.py
+++ launch/box_bot.launch.py
@@ -1,29 +1,37 @@
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import ThisLaunchFileDir
-from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
-import os
+from launch_ros.substitutions import FindPackageShare
+from launch.substitutions import PathJoinSubstitution
 
 def generate_launch_description():
-
     return LaunchDescription([
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(['/home/rhody/ros2_dev/src/hoverboard_ros2_control/hoverboard_demo_bringup/launch/hoverboard.launch.py'
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('hoverboard_control'), 'launch', 'hoverboard.launch.py'
+                ])
             ])
         ),
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(['/home/rhody/ros2_dev/src/nmea_navsat_driver/launch/nmea_serial_driver.launch.py'
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('py_qmc6310_driver'), 'launch', 'qmc6310.launch.py'
+                ])
             ])
         ),
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(['/home/rhody/ros2_dev/src/sllidar_ros2/launch/sllidar_c1_launch.py'
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('nmea_navsat_driver'), 'launch', 'nmea_serial_driver.launch.py'
+                ])
             ])
         ),
         IncludeLaunchDescription(
-            PythonLaunchDescriptionSource(['/home/rhody/ros2_dev/src/py_magnetometer_driver/launch/magnetometer.launch.py'
+            PythonLaunchDescriptionSource([
+                PathJoinSubstitution([
+                    FindPackageShare('sllidar_ros2'), 'launch', 'sllidar_c1_launch.py'
+                ])
             ])
         ),
-        # Include more launch files as needed 
     ])
